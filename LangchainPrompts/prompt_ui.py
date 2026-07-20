aa3d76efa3c3c00d1d2d77031ca89ea54f8cf677
mzaid622 <M.Zaid990447@gmail.com>
2026-07-20 11:52:45 +0500

    Details Understanding prompts

--- LangchainPrompts/prompt_ui.py
+++ LangchainPrompts/prompt_ui.py
@@ -1,16 +1,53 @@
-from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import load_prompt
 import streamlit as st
+from dotenv import load_dotenv
+import os
 
-llm = HuggingFacePipeline.from_model_id(
-    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    task="text-generation",
-    pipeline_kwargs=dict(temperature=0.5, max_new_tokens=100),
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct", task="text-generation"
 )
 model = ChatHuggingFace(llm=llm)
 
 st.header("Research Tool")
-user_input = st.text_input("Enter your prompt here:")
+
+paper_input = st.selectbox(
+    "Select Research Paper Name",
+    [
+        "Attention Is All You Need",
+        "BERT: Pre-training of Deep Bidirectional Transformers",
+        "GPT-3: Language Models are Few-Shot Learners",
+        "Diffusion Models Beat GANs on Image Synthesis",
+    ],
+)
+
+style_input = st.selectbox(
+    "Select Explanation Style",
+    ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"],
+)
+
+length_input = st.selectbox(
+    "Select Explanation Length",
+    [
+        "Short (1-2 paragraphs)",
+        "Medium (3-5 paragraphs)",
+        "Long (detailed explanation)",
+    ],
+)
+
+template = load_prompt("template.json")
+
 
 if st.button("Press"):
-    result = model.invoke(user_input)
+    chain = template | model
+
+    result = chain.invoke(
+        {
+            "paper_input": paper_input,
+            "style_input": style_input,
+            "length_input": length_input,
+        }
+    )
     st.write(result.content)
